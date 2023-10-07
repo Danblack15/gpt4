bcaa1822c45cac7906ba671abf97899866e40e53
Федоров Даниил <fedorov_danilka@bk.ru>
2023-10-07 17:59:57 +0500

    add: media > 768

--- local/templates/gpt4/layout/src/js/init/parallax.js
+++ local/templates/gpt4/layout/src/js/init/parallax.js
@@ -1,6 +1,8 @@
 import { Parallax } from '../components/parallax';
 
 window.addEventListener('load', () => {
+    if (window.innerWidth <= 1024) return;
+     
     const allParallaxItems = $.qsa('[data-parallax]');
     
     allParallaxItems?.forEach(parallaxItem => {
